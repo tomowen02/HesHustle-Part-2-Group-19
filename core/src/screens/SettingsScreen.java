7a1d84a0554eb52ce8e7f65037b0713f7a56ec03
Dan <dan@manby.org>
2024-05-08 15:28:16 +0100

    Removed unnecessary import.

--- core/src/screens/SettingsScreen.java
+++ core/src/screens/SettingsScreen.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
