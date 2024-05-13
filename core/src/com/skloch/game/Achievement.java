5a9e01626b246030df48aeea5c795c1719d0d62c
Dan <dan@manby.org>
2024-05-13 10:42:39 +0100

    Added Achievement class to keep track of different achievements and their conditions.

---
+++ core/src/com/skloch/game/Achievement.java
@@ -0,0 +1,45 @@
+package com.skloch.game;
+
+import java.util.HashMap;
+import java.util.function.Predicate;
+
+
+public class Achievement {
+    private final String name;
+    private final String description;
+    private boolean achieved;
+    private final HashMap<Event, Predicate<Event>> predicates;
+
+    public Achievement(String name, String description) {
+        this.name = name;
+        this.description = description;
+        achieved = false;
+        predicates = new HashMap<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isAchieved() {
+        return achieved;
+    }
+
+    public boolean validate() {
+        if (achieved) return true;
+
+        achieved = true;
+        for (Event event : predicates.keySet()) {
+            achieved = predicates.get(event).test(event);
+        }
+        return achieved;
+    }
+
+    public void addPredicate(Event event, Predicate<Event> predicate) {
+        predicates.put(event, predicate);
+    }
+}
