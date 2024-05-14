8849fce2521fb665903027efee789ff1ce5ff9db
Tom Owen <77887593+tomowen02@users.noreply.github.com>
2024-05-14 16:49:18 +0100

    New Maps and edits

--- core/src/com/skloch/game/EventManager.java
+++ core/src/com/skloch/game/EventManager.java
@@ -32,16 +32,16 @@ public class EventManager {
         // Define what to say when interacting with an object whose text won't change
         objectInteractions = new HashMap<String, Event>();
         objectInteractions.put("chest", new Event("chest", "Open the chest?", 0));
-        objectInteractions.put("comp_sci", new Event("comp_sci", "Study in the Computer Science building?", 10));
+        objectInteractions.put("comp_sci", new Event("comp_sci", "Would you like to study?", 20));
         objectInteractions.put("chat", new Event("chat", "Would you like to chat to your friends?", 10));
-        objectInteractions.put("basketball", new Event("basketball", "Would you like to play basketball?", 10));
+        objectInteractions.put("basketball", new Event("basketball", "Would you like to play basketball?", 20));
         objectInteractions.put("eat", new Event("eat", "Would you like to eat?", 10));
         objectInteractions.put("accomodation", new Event("accomodation", "Go to sleep for the night?\nYour alarm is set for 8am.", 0));
-        objectInteractions.put("rch", new Event("rch", "", 10)); // Changes, dynamically returned in getObjectInteraction
+        objectInteractions.put("rch", new Event("rch", "Would you like to study in the Ron Cook Hub?", 10)); // Changes, dynamically returned in getObjectInteraction
         objectInteractions.put("tree", new Event("tree", "Speak to the tree?", 5));
         objectInteractions.put("teleport", new Event("teleport", "Would you like to move location?", 0));
         objectInteractions.put("ducks", new Event("ducks", "Would you like to feed the ducks?", 10));
-        objectInteractions.put("cook", new Event("cook", "Would you like to cook?", 10));
+        objectInteractions.put("cook", new Event("cook", "Would you like to cook?", 30));
 
         // Some random topics that can be chatted about
         String[] topics = {"Dogs", "Cats", "Exams", "Celebrities", "Flatmates", "Video games", "Sports", "Food", "Fashion"};
@@ -93,6 +93,7 @@ public class EventManager {
                 compSciEvent(args);
                 break;
             case "rch":
+                compSciEvent(args);
                 break;
             case "accomodation":
                 accomEvent(args);
@@ -133,10 +134,7 @@ public class EventManager {
      */
     public String getObjectInteraction(String key) {
         if (key.contains("teleport")) {
-            key = "teleport";
-        }
-        if (key.equals("rch")) {
-            return String.format("Eat %s at the Ron Cooke Hub?", gameScreen.getMeal());
+            return "teleport";
         } else {
             return objectInteractions.get(key).getText();
         }
